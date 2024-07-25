8f7c7b50d8d859491c9b3797a5d912f3366263b3
iamarasekera <iamarasekera.dev@gmail.com>
2024-07-25 12:43:48 +0800

    Added the image upload component

--- components/ImageUploader.js
+++ components/ImageUploader.js
@@ -7,19 +7,23 @@ import '@uppy/core/dist/style.css';
 import '@uppy/dashboard/dist/style.css';
 
 const ImageUploader = () => {
-  const uppy = new Uppy({
+  const uppy = React.useMemo(() => new Uppy({
     meta: { type: 'avatar' },
     restrictions: { maxNumberOfFiles: 10, allowedFileTypes: ['image/*'] },
     autoProceed: false,
-  });
+  }), []);
 
-  uppy.use(AwsS3, {
-    companionUrl: '/api/s3-upload', // We'll create this endpoint
-  });
+  React.useEffect(() => {
+    uppy.use(AwsS3, {
+      companionUrl: '/api/s3-upload',
+    });
 
-  uppy.on('complete', (result) => {
-    console.log('Upload complete! We hve uploaded these files:', result.successful);
-  });
+    uppy.on('complete', (result) => {
+      console.log('Upload complete! We hve uploaded these files:', result.successful);
+    });
+
+    return () => uppy.close();
+  }, [uppy]);
 
   return (
     <Dashboard
